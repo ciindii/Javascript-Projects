6ca50c4df9c106275a6bcf12be1af0ff64da71b5
ciindii <cindythitruong@gmail.com>
2019-01-04 20:26:44 -0800

    added filtered names

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
-var greeting = (function (name) {
-  console.log('hello' + name);
-}('John'));
-
-var buy = (function (item, item2) {
-  console.log('You need to get ' + item + ' ' + item2);
-}('soap', 'sand'));
-
-var chicken = {
-  calorie: 140,
-  protien: 10,
-  fats: 5,
-};
-
-function item(food) {
-  var
-  food.push('saturated fats');
-}
-
-item(chicken);
-console.log(chicken.fats);
-
-
-git --version
-git config --list
-git config -- global user.name "cindy truong"
-git config --global user.email "email.com"
+// var greeting = (function (name) {
+//   console.log('hello' + name);
+// }('John'));
+//
+// var buy = (function (item, item2) {
+//   console.log('You need to get ' + item + ' ' + item2);
+// }('soap', 'sand'));
+//
+// var chicken = {
+//   calorie: 140,
+//   protien: 10,
+//   fats: 5,
+// };
+//
+// function item(food) {
+//   var
+//   food.push('saturated fats');
+// }
+//
+// item(chicken);
+// console.log(chicken.fats);
+//
+//
+// git --version
+// git config --list
+// git config -- global user.name "cindy truong"
+// git config --global user.email "email.com"
